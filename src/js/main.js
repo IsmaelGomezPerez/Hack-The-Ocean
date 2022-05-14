81a039334dd3ab5c0424ab288ca71b05feba80f8
Ismael Antonio Gomez Perez <ismaelgpay@gmail.com>
2022-05-14 17:16:11 -0500

    Actualizacion de textos y logo

--- src/js/main.js
+++ src/js/main.js
@@ -7,5 +7,14 @@ $(document).ready(function(){
             top:'0'
         }, 2000 + (index*500));
     });
-
+    if($(window).width() > 800){
+        $('header .textos').css({
+            opacity:0,
+            marginTop:0,
+        });
+        $('header .textos').animate({
+            opacity:1,
+            marginTop:'-52px'
+        },1500);
+    };
 });
