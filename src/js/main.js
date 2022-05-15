4618c695f2bd4b99e89988a1550a9a153e7ade30
Ismael Antonio Gomez Perez <ismaelgpay@gmail.com>
2022-05-15 16:39:27 -0500

    Agregando animaciones

--- src/js/main.js
+++ src/js/main.js
@@ -1,20 +1,35 @@
-// $(document).ready(function(){
-//     $('.menu a').each(function(index, elemento){
-//         $(this).css({
-//             'top':'-200px'
-//         });
-//         $(this).animate({
-//             top:'0'
-//         }, 2000 + (index*500));
-//     });
-//     if($(window).width() > 800){
-//         $('header .textos').css({
-//             opacity:0,
-//             marginTop:0,
-//         });
-//         $('header .textos').animate({
-//             opacity:1,
-//             marginTop:'-52px'
-//         },1500);
-//     };
-// });
+$(document).ready(function(){
+    $('.menu a').each(function(index, elemento){
+        $(this).css({
+            'top':'-200px'
+        });
+        $(this).animate({
+            top:'0'
+        }, 2000 + (index*500));
+    });
+    if($(window).width() > 800){
+        $('header .textos').css({
+            opacity:0,
+            marginTop:0,
+        });
+        $('header .textos').animate({
+            opacity:1,
+            marginTop:'-52px'
+        },1500);
+
+
+    };
+     
+    $('#btn-acerca-de').on('click',function(e){
+        e.preventDefault();
+        $('html,body').animate({
+            scrollTop:700
+        },500);
+    });
+    $('#btn-equipo').on('click',function(e){
+        e.preventDefault();
+        $('html,body').animate({
+            scrollTop:2300
+        },500);
+    });
+});
